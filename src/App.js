3f4904164842e86139bddd6a0afbe6d31850bea7
gaokehan <gaokehan@zhanhuo.com>
2023-07-18 17:35:12 +0800

    这次所有的 state 管理都由 Board 组件处理

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React, { useState } from 'react';
  * 点击时显示“X”
  * 需要在内部声明一个handleClick函数
  */
-function Square({value}) {
+function Square({value,onSquareClick}) {
   // 3 希望 Square 组件能够“记住”它被单击过，并用“X”填充它。为了“记住”一些东西，组件使用 state。
   // value : 存储值
   // setValue : 可用于更改值的函数
@@ -18,7 +18,7 @@ function Square({value}) {
   //   setValue('X')
   // }
   
-  return <button className='square'>{value}</button> // 2
+  return <button className='square' onClick={onSquareClick}>{value}</button> // 2
 }
 /**
  * 4 将游戏的 state 存储在 Board 父组件中，而不是每个 Square 中。Board 组件可以通过传递一个 props 来告诉每个 Square 显示什么
@@ -29,22 +29,29 @@ export default function Board() {
   // 数组中的每个元素对应于一个 square 的值。稍后填写棋盘时，squares 数组将如下所示： ['O', null, 'X', 'X', 'X', 'O', 'O', null, null]
   // 7 Board 组件需要将 value props 向下传递给它渲染的每个 Square
   // 8 编辑 Square 组件，以从 Board 组件接收 value props。这将需要删除 Square 组件自己的 value state 和按钮的 onClick props
+
+  function handleClick(i) {
+
+    const nextSquares = squares.slice()
+    nextSquares[i] = 'X'
+    setSquares(nextSquares)
+  }
   return (
     <div>
       <div className="board-row">
-        <Square value={squares[0]}/>
-        <Square value={squares[1]}/>
-        <Square value={squares[2]}/>
+        <Square value={squares[0]} onSquareClick={ () => handleClick(0) }/>
+        <Square value={squares[1]} onSquareClick={ () => handleClick(1) }/>
+        <Square value={squares[2]} onSquareClick={ () => handleClick(2) }/>
       </div>
       <div className="board-row">
-        <Square value={squares[3]}/>
-        <Square value={squares[4]}/>
-        <Square value={squares[5]}/>
+        <Square value={squares[3]} onSquareClick={ () => handleClick(3) }/>
+        <Square value={squares[4]} onSquareClick={ () => handleClick(4) }/>
+        <Square value={squares[5]} onSquareClick={ () => handleClick(5) }/>
       </div>
       <div className="board-row">
-        <Square value={squares[6]}/>
-        <Square value={squares[7]}/>
-        <Square value={squares[8]}/>
+        <Square value={squares[6]} onSquareClick={ () => handleClick(6) }/>
+        <Square value={squares[7]} onSquareClick={ () => handleClick(7) }/>
+        <Square value={squares[8]} onSquareClick={ () => handleClick(8) }/>
       </div>
     </div>
   )
