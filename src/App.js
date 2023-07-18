1b4d7633c36c6b596e83b74dbae45b89c3ecd3ab
gaokehan <gaokehan@zhanhuo.com>
2023-07-18 17:03:45 +0800

    创建交互click；用state存储每个方块的值，改变它

--- src/App.js
+++ src/App.js
@@ -2,28 +2,43 @@
 
 import React, { useState } from 'react';
 
+/**
+ * 创建交互性
+ * 点击时显示“X”
+ * 需要在内部声明一个handleClick函数
+ */
+function Square() {
+  // 3 希望 Square 组件能够“记住”它被单击过，并用“X”填充它。为了“记住”一些东西，组件使用 state。
+  // value : 存储值
+  // setValue : 可用于更改值的函数
+  const [value,setValue] = useState(null)
 
-function Square({ value }) {
-  return <button className='square'>{value}</button>
+  function handleClick() {  // 1
+    // 点击时更改为 X 
+    setValue('X')
+    
+  }
+  
+  return <button className='square' onClick={handleClick}>{value}</button> // 2
 }
 
 export default function Board() {
   return (
     <div>
       <div className="board-row">
-        <Square value={1}/>
-        <Square value={2}/>
-        <Square value={3}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={4}/>
-        <Square value={5}/>
-        <Square value={6}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={7}/>
-        <Square value={8}/>
-        <Square value={9}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </div>
   )
